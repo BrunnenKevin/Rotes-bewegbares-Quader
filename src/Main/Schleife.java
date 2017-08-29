6297c5aa4818ad68adafba9e5bda578525ebb16d
Kevin <Kevin@KevinsPC.Speedport_W_724V_Typ_A_05011603_05_020>
2017-08-29 16:58:56 +0200

    Rotes bewegliches Quader

---
+++ src/Main/Schleife.java
@@ -0,0 +1,33 @@
+
+package Main;
+
+public class Schleife {
+    
+    
+    public static void main(String[] args) {
+        
+        initialisieren();
+        
+        while(true){
+            berechnen();
+            
+            
+            draw();
+            
+        }
+         
+    }
+
+    private static void initialisieren() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void berechnen() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void draw() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
